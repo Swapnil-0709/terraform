c8c6b6944363351f2bfacdde26762e0fb1b88a94
swapnil <swapnildhk4@gmail.com>
2021-10-27 19:51:40 +0530

    terraform

--- lambda.tf
+++ lambda.tf
@@ -1,14 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
+variable "aws_access_key" {
+  default="AKIAVHWTWJ6TFVLT4EDH"
 }
-
+variable "aws_secret_key" {
+  default="+07qKu6nXgYROSNqFLzWg7VfSno3VwPPq9ZRBnIA"
+}
 provider "aws" {
   region  = "us-east-2"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
 data "archive_file" "lambda-zip" {
