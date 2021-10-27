ca20a5b1f0d87fb505669e34e9dcb9eb8911b9c6
swapnil <swapnildhk4@gmail.com>
2021-10-27 23:11:16 +0530

    terraform

--- lambda.py
+++ lambda.py
@@ -3,6 +3,6 @@ import json
 
 def lambda_handler(event, context):
     return{
-        'StatusCode': 200,
-        'body': json.dumos(event['headers']['X-forwarded-For'])
+        'statusCode': 200,
+        'body': json.dumps(event['headers']['X-forwarded-For'])
     }
