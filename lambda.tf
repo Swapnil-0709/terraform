50e28c8bf69f02bcabbf80474f97d51ed0df9612
swapnil <swapnildhk4@gmail.com>
2021-10-28 16:41:59 +0530

    terraform

--- lambda.tf
+++ lambda.tf
@@ -5,7 +5,7 @@ variable "aws_secret_key" {
   default="r8HEWrSAjTCjqxI/5scBlpDrK9UI7JYJwbeRQx7Z"
 }
 provider "aws" {
-  region  = "us-east-2"
+  region  = "us-east-1"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
 }
